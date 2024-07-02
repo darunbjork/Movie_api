d43afcccf234d7eb64857997490a3189e1e1dca0
Darun Bjork <139690402+darunbjork@users.noreply.github.com>
2024-07-02 17:03:37 +0200

    Added a localhost

--- server.js
+++ server.js
@@ -22,6 +22,7 @@ const Users = Models.User;
 // Define the allowed origins
 const allowedOrigins = [
   'http://localhost:8080',
+  'http://localhost:3000', //Added this localhost 
   'http://testsite.com',
   'http://localhost:1234',
   'http://localhost:4200',
